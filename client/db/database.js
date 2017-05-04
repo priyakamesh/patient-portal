0d7c0f7feacfd3fc8f19c4f1287652b03abd5f46
krishnapriya <krishnapriyakamesh@gmail.com>
2017-05-04 11:06:34 -0500

    added basic file structure, updated readme

---
+++ client/db/database.js
@@ -0,0 +1,17 @@
+// created this file manually so we can specify
+// the environment (test, development, or production),
+// require the knexfile.js, and export the configuration
+// (based on the environment) for our database:
+
+const environment = process.env.NODE_ENV || 'development';
+const config = require('../knexfile.js')[environment];
+const knex = require('knex')(config);
+const bookshelf = require('bookshelf')(knex);
+const cascadeDelete = require('bookshelf-cascade-delete');
+
+
+// Resolve circular dependencies with relations, ie models requiring each other
+// when defining relationships
+bookshelf.plugin('registry');
+
+module.exports = {knex, bookshelf}
