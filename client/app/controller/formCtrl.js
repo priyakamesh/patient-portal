ae8e98d5781a0b4c3a9467db0b8d45349ff5d265
krishnapriya <krishnapriyakamesh@gmail.com>
2017-05-10 09:21:16 -0500

    demographic form working fine

--- client/app/controller/formCtrl.js
+++ client/app/controller/formCtrl.js
@@ -1,4 +1,20 @@
-patient_portal.controller('FormCtrl', function($scope, AuthFactory,$location){
+patient_portal.controller('FormCtrl', function($scope, AuthFactory,$location, $http){
   $scope.currentUser = AuthFactory.getCurrentPatient()
-  console.log("$scope.currentUser",$scope.currentUser);
+  $scope.keydown = ()=>{
+    console.log("$scope.first_name",$scope.currentUser.firstname);
+  }
+  $scope.personal = () =>{
+      console.log("$scope.fdob",$scope.dob);
+    $http.patch(`http://localhost:3000/api/v1/patient/${$scope.currentUser.id}`,{
+      firstname: $scope.currentUser.firstname,
+      lastname: $scope.currentUser.last_name,
+      dob: $scope.currentUser.dob,
+      ethnicity: $scope.currentUser.ethnicity,
+      address: $scope.currentUser.address,
+      phonenumber: $scope.currentUser.phone
+    })
+    .then((data) =>{
+      console.log("updated in patients table")
+    })
+  }
 })
